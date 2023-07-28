03fccbc137cbe91e0fff62c753589b6357e7998d
Eugenio Gimenez <e.o.gimenez@gmail.com>
2023-07-28 20:53:21 +0200

    fdandole forma, probando nueva modulacion del useeffect

--- client/components/posts/PostCard.jsx
+++ client/components/posts/PostCard.jsx
@@ -1,11 +1,12 @@
 import './PostCard.css';
 
 export default function PostCard({ post }) {
+	const IMG_PREFIX = 'http://127.0.0.1:8000/';
 	return (
 		<section className='section--post--card'>
 			<h2>{post.title}</h2>
 			<h2>{post.content}</h2>
-			<h2>{post.image_url}</h2>
+			<img src={IMG_PREFIX + post.image_url} className='post--card--img'/>
 			<h2>{post.author}</h2>
 		</section>
 	);
